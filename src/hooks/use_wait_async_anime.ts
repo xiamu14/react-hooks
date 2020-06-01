b81b29b4d43ae918a4067e1ce2729cf249441a45
ben <544652471@qq.com>
2020-06-01 10:52:59 +0800

    feat: add new hooks : useWaitAsyncAnime

---
+++ src/hooks/use_wait_async_anime.ts
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * @description 等待异步事件反馈的动画 hook
+ * @param delay 动画延迟时间, 单位 秒
+ * @param cycle 动画周期，单位 秒
+ */
+export default function useWaitAsyncAnime(delay: number, cycle: number) {
+  // 预启动动画标识
+  const [tag, setTag] = useState(false);
+  const [isPlay, setIsPlay] = useState(false); // 动画运行标识
+  const [startTime, setStartTime] = useState<number>(0);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+    if (tag) {
+      timer = setTimeout(() => {
+        clearTimeout(timer);
+
+        if (tag) {
+          setIsPlay(true);
+          setStartTime(Date.now());
+        }
+      }, delay * 1000);
+    }
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [tag, delay]);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (!tag && isPlay) {
+      if (Date.now() - startTime < cycle * 1000) {
+        const duration = cycle * 1000 - (Date.now() - startTime);
+        timer = setTimeout(() => {
+          clearTimeout(timer);
+
+          setIsPlay(false);
+        }, duration);
+      } else {
+        setIsPlay(false);
+      }
+    }
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+    };
+  }, [tag, isPlay, cycle, startTime]);
+
+  return { isPlay, setIsPlay, tag, setTag };
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,5 @@ export { default as useForceUpdate } from "./hooks/force_update";
 export { default as useResetCheck } from "./hooks/reset_check";
 
 export { default as useTableBodyHeight } from "./hooks/table_body_height";
+
+export { default as useWaitAsyncAnime } from "./hooks/use_wait_async_anime";
